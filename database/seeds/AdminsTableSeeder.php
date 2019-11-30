8ef1c41e6660ab9a7197f83779bd3ae1dcc851a1
tudechristian3 <tudechristian3@gmail.com>
2019-11-30 11:19:08 +0800

    first commit

---
+++ database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        Admin::create([
+            'name'      =>  $faker->name,
+            'email'     =>  'admin@admin.com',
+            'password'  =>  bcrypt('password'),
+        ]);
+    }
+}
